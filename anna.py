57154279ff28f4b2a3f2068f8932dcfba9fb5ac0
Anna Vahtera <anna@vahtera.org>
2025-02-26 21:59:31 +0200

    Add files via upload
    Adding files

---
+++ anna.py
@@ -0,0 +1,8 @@
+def clearScreen(): # Clear Screen
+    system('cls' if name == 'nt' else 'clear') # Clear Screen depending on OS
+
+def openFile(fName):
+    tList = []
+    with open(fName, "r", encoding="utf-8") as f: # Open the File and Read the Lines into an Array
+        tList = [line.strip() for line in f]
+    return tList
