69668cb7d9fdbdc4c90b0d81de0fe32d5c08f8e6
Anna Vahtera <anna@vahtera.org>
2025-02-26 23:26:29 +0200

    Update anna.py

--- anna.py
+++ anna.py
@@ -9,7 +9,16 @@ from os import system, name
 def clearScreen(): # Clear Screen
     system('cls' if name == 'nt' else 'clear') # Clear Screen depending on OS
 
+def clear_screen(): # Clear Screen
+    system('cls' if name == 'nt' else 'clear') # Clear Screen depending on OS
+
 def openFile(fName):
+    tList = []
+    with open(fName, "r", encoding="utf-8") as f: # Open the File and Read the Lines into an Array
+        tList = [line.strip() for line in f]
+    return tList
+
+def open_file(fName):
     tList = []
     with open(fName, "r", encoding="utf-8") as f: # Open the File and Read the Lines into an Array
         tList = [line.strip() for line in f]
