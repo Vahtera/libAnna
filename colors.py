c637cf9816b8ecdc554b288da0a3d337dcedcb20
Anna Vahtera <anna@vahtera.org>
2025-02-27 08:55:40 +0200

    Added color definition module.

---
+++ colors.py
@@ -0,0 +1,14 @@
+'''ANSI Color Definitions'''
+
+# Color Definitions
+WHITE = "\033[37m"  # White Text Color
+BLUE = "\033[34m"  # Blue Text Color
+YELLOW = "\033[33m"  # Yellow Text Color
+GREEN = "\033[32m"  # Green Text Color
+RED = "\033[31m"  # Red Text Color
+CYAN = "\033[36m"  # Cyan Text Color
+PURPLE = "\033[35m"  # Purple Text Color
+BLACK = "\033[30m"  # Black Text Color
+BOLD = "\033[1m"  # Bold Text
+NOBOLD = "\033[22m"  # No Bold Text
+ENDC = "\033[0m"  # Reset Text Color
