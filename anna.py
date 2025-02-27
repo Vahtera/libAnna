5fd6ea038d072c52aed08241ab70323ff618af7e
Anna Vahtera <anna@vahtera.org>
2025-02-27 02:12:41 +0200

    Update anna.py

--- anna.py
+++ anna.py
@@ -6,19 +6,23 @@
 
 from os import system, name
 
-def clearScreen(): # Clear Screen
+def clearScreen(): # Compatibility, Deprecated.
+    '''Clear Screen'''
     system('cls' if name == 'nt' else 'clear') # Clear Screen depending on OS
 
-def clear_screen(): # Clear Screen
+def clear_screen():
+    '''Clear Screen'''
     system('cls' if name == 'nt' else 'clear') # Clear Screen depending on OS
 
-def openFile(fName):
+def openFile(fName): # Compatibility, Deprecated.
+    ''Open file in UTF-8'''
     tList = []
     with open(fName, "r", encoding="utf-8") as f: # Open the File and Read the Lines into an Array
         tList = [line.strip() for line in f]
     return tList
 
 def open_file(fName):
+    '''Open file in UTF-8 encoding'''
     tList = []
     with open(fName, "r", encoding="utf-8") as f: # Open the File and Read the Lines into an Array
         tList = [line.strip() for line in f]
