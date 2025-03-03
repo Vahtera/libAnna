332a828b5f3e6dc3381a792dd566205475d06208
Anna Vahtera <anna@vahtera.org>
2025-03-03 23:51:56 +0200

    Adding C# code to the library.

---
+++ libAnnaC.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace libAnnaC
+{
+    public static class libAnna
+    {
+        public static string Capitalize(this string s)
+        {
+            if (string.IsNullOrEmpty(s))
+                return s;
+            if (s.Length == 1)
+                return s.ToUpper();
+            return s.Remove(1).ToUpper() + s.Substring(1);
+        }
+    }
+}
